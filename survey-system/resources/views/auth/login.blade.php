8bc29c2e214766a07d1adafee735bd1c6d0b34a0
Khang Pham <kpham@upei.ca>
2022-01-17 00:19:49 -0400

    MVC set up

---
+++ survey-system/resources/views/auth/login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body  >
+    Login Page ss
+    <form >
+    <div class="form-group" class="bg-gray-300">
+        <label for="inputEmail">Email</label>
+        <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+    </div>
+    <div class="form-group">
+        <label for="inputPassword">Password</label>
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label class="form-check-label"><input type="checkbox"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+</body>
+</html>
