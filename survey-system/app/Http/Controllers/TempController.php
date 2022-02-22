7c0ff3faae6e189eedd37eef9c194b2558c5631b
Mohammed Lahloub <malahloub@upei.ca>
2022-02-21 22:03:08 -0400

    Added a controller for each task, no background

--- survey-system/app/Http/Controllers/TempController.php
+++ survey-system/app/Http/Controllers/TempController.php
@@ -10,23 +10,10 @@ class TempController extends Controller
         return view('welcome');
     }
 
-    public function home (){
-        return view('home');
-    }
+    
+    
 
-    public function survey_list (){
-        return view('survey_list');
-    }
+    
 
-    public function template () {
-        return view('template');
-    }
-
-    public function make_survey () {
-        return view('make_survey');
-    }
-
-    public function profile_settings () {
-        return view('profile_settings');
-    }
+    
 }
