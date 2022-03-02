4efdce0856e362b0a6836601cd20a9f3e231551e
Tianyu <tcao@upei.ca>
2022-03-02 10:47:01 -0500

    only display survey list

--- survey-system/app/Http/Controllers/SurveyListController.php
+++ survey-system/app/Http/Controllers/SurveyListController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class SurveyListController extends Controller
 {
-    public function index (){
-        return view('survey_list');
-    }
+        public function index (){
+
+            //       $Survey_list = new Survey_list;
+                    $survey = Survey_list::where('user_id', 100)->paginate(15);
+                    return view('survey_list',compact('survey'));
+            
+        }
 }
---
+++ survey-system/app/Models/Survey_list.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Survey_list extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+//     use HasFactory;
+    public $table = 'survey_list';
+//     protected $fillable = [
+//         'problem',
+//         'user_id',
+//         'updated_at',
+//         'created_at'
+//     ];
+
+//    public $timestamps = false;
+}
