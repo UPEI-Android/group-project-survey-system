7f49480e83ee06bb91da0fc27df2c2ce5f98a188
Tianyu <tcao@upei.ca>
2022-03-10 10:43:30 +0800

    another update

--- survey-system/app/Http/Controllers/SurveyListController.php
+++ survey-system/app/Http/Controllers/SurveyListController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class SurveyListController extends Controller
 {
     public function index (){
-        $survey_list = DB::table('surveys')->get();
+        $uid=Auth::user()->id;
+        $survey_list = DB::table('surveys')->where('profiles_id',$uid)->get();
         // print_r($survey_list);die;
         return view('survey_list',compact('survey_list'));
     }
