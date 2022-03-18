11a5b6171915cd8ac5e5849c03d3988dbcb7b816
Tianyu <tcao@upei.ca>
2022-03-18 10:43:04 +0800

    excelConvert

---
+++ survey-system/app/Http/Controllers/SurveyConsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SurveyConsController extends Controller
+{
+    function getData(){
+        return "Form Data will be here";
+    }
+}
