5f823d1f03cce5d395ee437b551d2951aa3556a3
oo <kun@oodeMacBook-Air.local>
2022-03-27 22:01:33 -0300

    success page

--- survey-system/resources/views/success.blade.php
+++ survey-system/resources/views/success.blade.php
@@ -2,7 +2,7 @@
     <head>
     <link href="/css/success.css" rel="stylesheet">
     <div class='content'>
-  <img src = '/css/successicon.png '></img>
+  <img src = '/css/successicon.png' alt="Responsive image"></img>
 </br>
   <div class='title'>Thank you!</div>
   <div class='text'>Your answer has been received!</div>
